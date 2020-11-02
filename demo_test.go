6a2ca4f39006de583d585009ca45987edae11923
qixi <qixi@beihai365.com>
2020-11-02 16:35:38 +0800

    添加一个爬虫得到的接口

--- demo_test.go
+++ demo_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/FlashFeiFei/yuque/request"
+	"github.com/FlashFeiFei/yuque/request/front"
 	"github.com/FlashFeiFei/yuque/response"
 	"io/ioutil"
 	"log"
@@ -50,11 +51,30 @@ func UserInfo2(w http.ResponseWriter, req *http.Request) {
 	log.Println(string(data))
 }
 
+//前端调用的api,非文档的，以后可能会被封掉
+func DocDetail(w http.ResponseWriter, req *http.Request) {
+	client := http.Client{}
+
+	creq, _ := http.NewRequest(http.MethodGet, "https://www.yuque.com/api/docs/tqzqet?book_id=1955564", nil)
+	resp, _ := client.Do(creq)
+	body, _ := ioutil.ReadAll(resp.Body)
+	log.Println(string(body))
+}
+
+func DocDetail2(w http.ResponseWriter, req *http.Request) {
+	log.Println("DocDetail2")
+	response := front.GetDocIntorSerializer("tqzqet", 1955564)
+	data, err := json.Marshal(response)
+	log.Println(string(data), err)
+}
+
 func TestRun(t *testing.T) {
 
 	http.HandleFunc("/", HelloServer)
 	http.HandleFunc("/user", UserInfo)
 	http.HandleFunc("/myuser", UserInfo2)
+	http.HandleFunc("/DocDetail", DocDetail)
+	http.HandleFunc("/DocDetail2", DocDetail2)
 
 	http.ListenAndServe(":12345", nil)
 }
